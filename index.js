96611f6d00993f81d44fb3e76058b277f9892f77
Stephen Osunrinde <47150226+itsYoungFox@users.noreply.github.com>
2021-04-28 16:42:28 +0100

    Major update

--- index.js
+++ index.js
@@ -158,7 +158,13 @@ user_nsp.on('connection', (socket) => {
               pLoad['chunkPROGRESS'] = data.payload['PROG'];
               pLoad['chunkTIME'] = data.payload['TELAPSED'];
 
-          request('https://5538533d1d0f.ngrok.io/JDS/req/req_handler.php?rtDownloadProg=true',
+          let urlParm = "?";
+          for (var key in pLoad) {
+            if (urlParm.length > 1) urlParm += '&';
+            urlParm += key+'='+pLoad[key];
+          }
+
+          request('https://5538533d1d0f.ngrok.io/JDS/req/req_handler.php'+urlParm,
           function(err, httpResponse, body){
             if (err) admin_server_nsp.emit('msg', {socket_type: 'user', socket_data: "ERROR => "+err});
             admin_server_nsp.emit('msg', {socket_type: 'user', socket_data: "RESPONSE => "+JSON.stringify(httpResponse)});
