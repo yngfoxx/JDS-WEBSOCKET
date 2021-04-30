9b4c0676e826b7726bb1beb9c3a3ffb69f7b97e8
Stephen Osunrinde <47150226+itsYoungFox@users.noreply.github.com>
2021-04-30 23:17:20 +0100

    Updated server domain

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ user_nsp.on('connection', (socket) => {
           admin_server_nsp.emit('msg', {socket_type: 'user', socket_data: "URL => "+urlParm});
 
           // UPDATE DOMAIN HERE TO GET DOWNLOAD PROGRESS AND SIZE
-          request('https://11272b98f115.ngrok.io/JDS/req/req_handler.php'+urlParm,
+          request('https://a2e7059239a5.ngrok.io/JDS/req/req_handler.php'+urlParm,
           function(err, httpResponse, body){
             if (err) admin_server_nsp.emit('msg', {socket_type: 'user', socket_data: "ERROR => "+err});
 
